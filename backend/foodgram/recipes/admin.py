19a1325061b8dc1309902dffd960fd7f8c27cc1b
Ilya Savinkin <iasavinkin@gmail.com>
2023-03-25 12:24:50 +0300

    stable admin site without users model

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
-from recipes.models import Ingredient, Recipe, Tag, Subscription
+from recipes.models import (Ingredient, InrgedientQuantity, Recipe,
+                            Subscription, Tag)
+
+
+class InrgedientQuantityInline(admin.TabularInline):
+    model = InrgedientQuantity
+    fields = ('ingredient', 'quantity')
+    extra = 1
+    verbose_name = 'Ингредиент'
 
 
 class RecipeAdmin(admin.ModelAdmin):
-    feilds = ['name', 'author']
-admin.site.register([Ingredient, Tag, Subscription])
+    list_display = ('name', 'author')
+    search_fields = ('name',)
+    list_filter = ('author', 'tags',)
+    inlines = (InrgedientQuantityInline,)
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    fields = ('name', 'measurement_unit')
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('colored', 'slug')
+
+
+admin.site.register(Subscription)
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Tag, TagAdmin)
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.html import format_html
 
 User = get_user_model()
 
@@ -8,6 +9,9 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=250, verbose_name='Название')
     measurement_unit = models.CharField(max_length=10,
                                         verbose_name='Единицы измерения')
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'
 
     class Meta:
         verbose_name = 'Ингредиент'
@@ -19,13 +23,20 @@ class Tag(models.Model):
     name = models.CharField(null=False, blank=False,
                             max_length=250, verbose_name='Название')
     color = models.CharField(null=False, blank=False,
+                             default='#ffffff',
                              max_length=7, verbose_name='Цвет в HEX')
     slug = models.SlugField(null=False, blank=False,
                             unique=True)
 
-    def __str__(self):
-        return self.name
+    def colored(self):
+        return format_html(
+            '<span style="color: {};">{}</span>',
+            self.color,
+            self.name,
+        )
 
+    def __str__(self):
+        return str(self.name)
 
     class Meta:
         verbose_name = 'Тэг'
@@ -38,11 +49,13 @@ class Recipe(models.Model):
                                verbose_name='Автор')
     name = models.CharField(max_length=250, null=False, blank=False,
                             verbose_name='Название')
-    image = models.ImageField(null=False, blank=False,
-                              verbose_name='Картинка')
+    image = models.ImageField(
+        null=True, blank=True,
+        verbose_name='Картинка')
     text = models.TextField(null=False, blank=False,
                             verbose_name='Текстовое описание')
     ingredients = models.ManyToManyField(Ingredient,
+                                         through='InrgedientQuantity',
                                          verbose_name='Ингредиенты')
     tags = models.ManyToManyField(Tag, verbose_name='Теги')
     cooking_time = models.IntegerField(
@@ -57,6 +70,19 @@ class Recipe(models.Model):
         ordering = ('pub_date',)
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
+
+
+class InrgedientQuantity(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE,
+                                   verbose_name='Ингредиент')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    quantity = models.DecimalField(decimal_places=2,
+                                   max_digits=6,
+                                   blank=False, null=False,
+                                   verbose_name='Количество')
+
+    def __str__(self):
+        return str(self.id)
 
 
 class Subscription(models.Model):
